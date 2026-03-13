10da8e999e904d5cf735375e4bc4ec10f400e541
Keyur Sapara <133037462+KeyurSapara@users.noreply.github.com>
2026-03-13 17:21:10 +0530

    Update code.groovy

--- vars/code.groovy
+++ vars/code.groovy
@@ -1,3 +1,5 @@
-echo 'code part'
-git url: 'https://github.com/KeyurSapara/TWS-django-notes-app-fork.git', branch: 'main'
-echo 'successfully cloned'
+def call (){
+    echo 'code part'
+    git url: 'https://github.com/KeyurSapara/TWS-django-notes-app-fork.git', branch: 'main'
+    echo 'successfully cloned'
+}
